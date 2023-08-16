f31b386c0c4acb5617a7209991064ed89dc5a263
Nand <1india.kng@gmail.com>
2023-08-17 00:24:30 +0530

    eureka-client&server

--- order-service/src/main/java/com/programming/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/programming/orderservice/OrderServiceApplication.java
@@ -2,12 +2,14 @@ package com.programming.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.programming.orderservice.*"})
 @EnableJpaRepositories
+@EnableEurekaClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
